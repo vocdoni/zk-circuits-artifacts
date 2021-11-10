1030be11bf997b04dd8971c47dbea3ede1f43494
arnaucube <root@arnaucube.com>
2021-11-10 19:06:59 +0100

    Add gen-dev-artifacts.sh to generate dev files

---
+++ gen-dev-artifacts.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+# Script to generate the dev-enviorment circuit-artifacts files (compile & trusted setup)
+
+SNARKJS=./node_modules/.bin/snarkjs 
+CIRCOM=circom 
+BUILD=build
+POWERSOFTAU=powersoftau
+DEVINFOFILE=dev-circuits-info.md
+
+powers_of_tau() {
+	echo "computing powers_of_tau"
+	mkdir $POWERSOFTAU
+	itime="$(date -u +%s)"
+	$SNARKJS powersoftau new bn128 15 $POWERSOFTAU/pot_0000.ptau -v
+	$SNARKJS powersoftau contribute $POWERSOFTAU/pot_0000.ptau $POWERSOFTAU/pot_0001.ptau --name=contribution -v -e=random
+	$SNARKJS powersoftau beacon $POWERSOFTAU/pot_0001.ptau $POWERSOFTAU/pot_beacon.ptau 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n=Final
+	$SNARKJS powersoftau prepare phase2 $POWERSOFTAU/pot_beacon.ptau $POWERSOFTAU/pot_final.ptau -v
+	$SNARKJS powersoftau verify $POWERSOFTAU/pot_final.ptau
+	ftime="$(date -u +%s)"
+	echo "powers_of_tau done in ($(($(date -u +%s)-$itime))s)"
+}
+
+compile_and_ts() {
+	CIRCUITCODE="pragma circom 2.0.0;
+
+include \"../../../node_modules/voccircuits/circuits/census.circom\";
+
+component main {public [processId, censusRoot, nullifier, voteHash]}= Census($NLEVELS);"
+
+	mkdir -p $CIRCUITPATH/$BUILD
+	echo "$CIRCUITCODE" > $CIRCUITPATH/circuit.circom
+
+	# reuse already computed powers of tau
+	cp $POWERSOFTAU/pot_final.ptau $CIRCUITPATH/$BUILD/pot_final.ptau
+
+
+	# compile circuit
+	echo "compiling the circuit"
+	itime="$(date -u +%s)"
+	circom $CIRCUITPATH/circuit.circom --r1cs --wasm --sym
+	mv circuit.* $CIRCUITPATH/$BUILD/
+	mv circuit_js $CIRCUITPATH/
+
+	ftime="$(date -u +%s)"
+	echo "circuit compiled in ($(($(date -u +%s)-$itime))s)"
+	mv $CIRCUITPATH/circuit_js/circuit.wasm $CIRCUITPATH/circuit.wasm
+
+	# compute trusted setup
+	echo "computing the trusted setup"
+	itime="$(date -u +%s)"
+	$SNARKJS zkey new $CIRCUITPATH/$BUILD/circuit.r1cs $CIRCUITPATH/$BUILD/pot_final.ptau $CIRCUITPATH/$BUILD/circuit_0000.zkey
+	$SNARKJS zkey contribute $CIRCUITPATH/$BUILD/circuit_0000.zkey $CIRCUITPATH/$BUILD/circuit_0001.zkey --name=contributor -v -e=random2
+	$SNARKJS zkey verify $CIRCUITPATH/$BUILD/circuit.r1cs $CIRCUITPATH/$BUILD/pot_final.ptau $CIRCUITPATH/$BUILD/circuit_0001.zkey
+	$SNARKJS zkey beacon $CIRCUITPATH/$BUILD/circuit_0001.zkey $CIRCUITPATH/circuit_final.zkey 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n=Final
+	$SNARKJS zkey verify $CIRCUITPATH/$BUILD/circuit.r1cs $CIRCUITPATH/$BUILD/pot_final.ptau $CIRCUITPATH/circuit_final.zkey
+	$SNARKJS zkey export verificationkey $CIRCUITPATH/circuit_final.zkey $CIRCUITPATH/verification_key.json
+	ftime="$(date -u +%s)"
+	echo "trusted setup done in ($(($(date -u +%s)-$itime))s)"
+
+	# store circuit info
+	echo "## Circuit $CIRCUITPATH leafs ($NLEVELS levels)\n" >> $DEVINFOFILE
+	$SNARKJS r1cs info $CIRCUITPATH/$BUILD/circuit.r1cs >> $DEVINFOFILE
+}
+
+if [ -d "$POWERSOFTAU" ]; then
+	echo "powers of tau already exist, avoid computing it"
+else
+	echo "powers of tau does not exist, compute it"
+	powers_of_tau
+fi
+
+touch $DEVINFOFILE
+
+NLEVELS=3
+NAME=$(echo "2 ^ $NLEVELS" | bc)
+CIRCUITPATH=zkcensusproof/dev/$NAME
+echo "compile_and_ts() of $CIRCUITPATH"
+compile_and_ts
+
+NLEVELS=4
+NAME=$(echo "2 ^ $NLEVELS" | bc)
+CIRCUITPATH=zkcensusproof/dev/$NAME
+echo "compile_and_ts() of $CIRCUITPATH"
+compile_and_ts
+
+NLEVELS=10
+NAME=$(echo "2 ^ $NLEVELS" | bc)
+CIRCUITPATH=zkcensusproof/dev/$NAME
+echo "compile_and_ts() of $CIRCUITPATH"
+compile_and_ts
+
+NLEVELS=16
+NAME=$(echo "2 ^ $NLEVELS" | bc)
+CIRCUITPATH=zkcensusproof/dev/$NAME
+echo "compile_and_ts() of $CIRCUITPATH"
+compile_and_ts
